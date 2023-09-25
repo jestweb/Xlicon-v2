41e7313064f9285d8f9f2c9918f981bcbda7c2df
jestweb <143500529+jestweb@users.noreply.github.com>
2023-09-25 06:23:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
-global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.Owner = ["2348050719993 ","2348050719993"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["2348050719993","2348050719993"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["2348050719993"];
+global.OwnerName = "Dōkeshi JesTer";
+global.BotName = "?ManDroid";
+global.packname = "Mandroiddd Bot";                             //Do not change.
+global.author = "By: jester";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
 
